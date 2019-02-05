edf1e79a22d481cf236806e9adf1db47e7859aca
Renan Teixeira <reteixeira@viceri.com.br>
2019-02-05 14:56:59 -0200

    começando componente de adicionar livros

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+//allow cross-origin request
+app.use(cors());
 
 mongoose.connect('mongodb://renant:123Aa321@ds221645.mlab.com:21645/learn-graphql')
 mongoose.connection.once('open', () => {
